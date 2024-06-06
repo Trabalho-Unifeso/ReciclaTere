8ee4d85d77aaa20e1e8bd93119e2262f347c70a7
Roosevelt Daflon <rooseveltdaflon@hotmail.com>
2024-06-06 19:20:05 -0300

    Update Perfil.tsx

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,89 +1,127 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, KeyboardAvoidingView, ScrollView, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, KeyboardAvoidingView, ScrollView, Modal, Alert } from 'react-native';
 import { NavegatorService } from '../service/NavegatorService';
 import Navbar from '../components/NavBar';
-
-interface User {
-  Nome: string;
-  Sobrenome: string;
-  Nascimento?: string;
-  Endereco?: string;
-  Telefone?: string;
+interface Usuario {
+  Email: string;
+  CodigoEmail: string;
+  NovaSenha?: string;
+  ConfirmarSenha?: string;
 }
 
-interface UserProfileProps {
-  user: User;
+interface PerfilUsuarioProps {
+  usuario: Usuario;
   onBackPress: () => void;
-  onSave: (updatedUser: User) => void;
+  onSave: (updatedUser: Usuario) => void;
 }
 
-export default function Perfil(NavegatorService: NavegatorService ) {
-  const [user, setUser] = useState<User>({
-    Nome: "Sergio",
-    Sobrenome: "Quintanilha",
-    Nascimento: "09/01/1937",
-    Endereco: "Rua 1, próximo a rua 2",
-    Telefone: "(21) 98883-3338"
+export default function Perfil(NavegatorService: NavegatorService) {
+  const [usuario, setUsuario] = useState<Usuario>({
+    Email: "",
+    CodigoEmail: "",
+    NovaSenha: "",
+    ConfirmarSenha: "",
   });
+  const [modalErroVisivel, setModalErroVisivel] = useState(false);
+  const [modalSucessoVisivel, setModalSucessoVisivel] = useState(false);
+
+  const validarSenhas = () => {
+    if (usuario.NovaSenha !== usuario.ConfirmarSenha) {
+      setModalErroVisivel(true);
+    } else {
+      setModalSucessoVisivel(true);
+    }
+  };
 
   return (
-    <View>
-      <View style={styles.container}>
-        <ScrollView>
-          <Image style={styles.LoginImage} source={require('../assets/Img/person-placeholder.jpg')} />
-          <View style={styles.userInfo}>
-            <View style={styles.inputContainer}>
-              <Text>Nome:</Text>
-              <TextInput
-                style={styles.input}
-                value={user.Nome}
-                onChangeText={(text) => setUser({ ...user, Nome: text })}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <Text>Sobrenome:</Text>
-              <TextInput
-                style={styles.input}
-                value={user.Sobrenome}
-                onChangeText={(text) => setUser({ ...user, Sobrenome: text })}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <Text>Data de Nascimento:</Text>
-              <TextInput
-                style={styles.input}
-                value={user.Nascimento}
-                onChangeText={(text) => setUser({ ...user, Nascimento: text })}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <Text>Endereço:</Text>
-              <TextInput
-                style={styles.input}
-                value={user.Endereco}
-                onChangeText={(text) => setUser({ ...user, Endereco: text })}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <Text>Telefone:</Text>
-              <TextInput
-                style={styles.input}
-                value={user.Telefone}
-                onChangeText={(text) => setUser({ ...user, Telefone: text })}
-              />
-            </View>
-            <TouchableOpacity style={styles.saveButton}>
-              <Text style={styles.saveButtonText}>Salvar</Text>
+    <KeyboardAvoidingView style={styles.container}>
+      <ScrollView>
+        <View style={styles.userInfo}>
+          <View style={styles.inputContainer}>
+            <Text>Digite seu e-mail:</Text>
+            <TextInput
+              style={styles.input}
+              value={usuario.Email}
+              onChangeText={(text) => setUsuario({ ...usuario, Email: text })}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text>Informe o código enviado no e-mail:</Text>
+            <TextInput
+              style={styles.input}
+              value={usuario.CodigoEmail}
+              onChangeText={(text) => setUsuario({ ...usuario, CodigoEmail: text })}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text>Digite sua nova senha:</Text>
+            <TextInput
+              style={styles.input}
+              value={usuario.NovaSenha}
+              onChangeText={(text) => setUsuario({ ...usuario, NovaSenha: text })}
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text>Confirme sua nova senha:</Text>
+            <TextInput
+              style={styles.input}
+              value={usuario.ConfirmarSenha}
+              onChangeText={(text) => setUsuario({ ...usuario, ConfirmarSenha: text })}
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity style={styles.saveButton} onPress={validarSenhas}>
+            <Text style={styles.saveButtonText}>Salvar</Text>
+          </TouchableOpacity>
+        </View>
+        <Navbar route={NavegatorService.navigation.navigate}></Navbar>
+      </ScrollView>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalErroVisivel}
+        onRequestClose={() => {
+          setModalErroVisivel(!modalErroVisivel);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>As senhas não coincidem!</Text>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalErroVisivel(!modalErroVisivel)}
+            >
+              <Text style={styles.textStyle}>OK</Text>
             </TouchableOpacity>
           </View>
-        </ScrollView>
-        
+        </View>
+      </Modal>
 
-      </View>
-      <Navbar route={NavegatorService.navigation.navigate}></Navbar>
-    </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalSucessoVisivel}
+        onRequestClose={() => {
+          setModalSucessoVisivel(!modalSucessoVisivel);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Senha alterada com sucesso!</Text>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalSucessoVisivel(!modalSucessoVisivel)}
+            >
+              <Text style={styles.textStyle}>OK</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </KeyboardAvoidingView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -109,15 +147,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
   },
-  LoginImage: {
-    borderColor: "darkgrey",
-    borderRadius: 100,
-    width: 150,
-    height: 150,
-    borderWidth: 2,
-    paddingStart: -25,
-    alignSelf: 'center'
-  },
   saveButton: {
     backgroundColor: '#007bff',
     padding: 10,
@@ -128,5 +157,43 @@ const styles = StyleSheet.create({
   saveButtonText: {
     color: '#fff',
     fontSize: 16,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
